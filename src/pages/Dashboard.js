f772551b08360ac25145dd72595f636459c5fadb
coriemaster <coriemaster@gmail.com>
2022-04-20 22:20:03 +0300

    change layout

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -14,6 +14,7 @@ import gameImg from '../assets/images/playpage/game.png';
 import BetTypeContainer from './playpage/BetTypeContainer';
 import ChatAndHistory from './playpage/ChatAndHistory';
 import Players from './playpage/Players';
+import { Col, Row } from "react-bootstrap";
 
 export const Dashboard = () => {
   const data = {
@@ -81,87 +82,92 @@ export const Dashboard = () => {
 
   return (
     <div className="dashboard">
-      
-      
-        
-          <div style={{ height: "100%" }}>
-            <div style={{ height: "calc(100% - 64px)", overflowY: "scroll" }}>
-              <div className="d-flex card-section">
-                <div className="cards-container">
-                  
-                  
-                  <div
-                    className="w-100 d-flex flex-column p-4"
-                    style={{ gridRow: "span 2" }}
-                  >
-                    <div className="game-content">
-                      <img src={gameImg}></img>
+      <div style={{ height: "100%" }}>
+        <div style={{ height: "calc(100% - 64px)", overflowY: "scroll" }}>
+          <div className="d-flex card-section">
+            <div className="cards-container">
+              <Row style={{width: '100%'}}>
+                <Col sm={12} md={9}>
+                  <div className="d-flex mb-2 flex-column md:flex-row">
+                    <Row>
+                      <Col sm={12} md={8}>
+                        <div
+                          className="d-flex flex-1 flex-column mb-2 md:mb-0"
+                        >
+                          <div className="game-content">
+                            <img src={gameImg}></img>
+                          </div>
+                        </div>
+                      </Col>
+                      <Col sm={12} md={4}>
+                        <div
+                          className="d-flex flex-column mb-2 md:ml-2 md:mb-0"
+                        >
+                          <BetTypeContainer/>
+                        </div>
+                      </Col>
+                    </Row>
+                  </div>
+                  <div className="flex-1 d-flex flex-column">
+                    <div className="d-flex flex-column wide d-flex flex-column">
+                      <ChatAndHistory/>  
                     </div>
                   </div>
-                  <div
-                    className="w-100 d-flex flex-column"
-                    style={{ gridRow: "span 2" }}
-                  >
-                    <BetTypeContainer/>
-                  </div>
-                  <div className="w-100 d-flex flex-column">
-                    <Players/>  
-                  </div>
-                  <div className="w-100 d-flex flex-column wide d-flex flex-column">
-                    <ChatAndHistory/>  
-                  </div>
-                  
-                </div>
-              </div>
-              <footer className="footer">
-                <div className="d-flex align-items-center">
-                  <CDBLink to="/" className="text-dark footer-link">
-                    <img alt="logo" src="/img/pages/logo.png" width="25px" />
-                    <span className="ml-4 lead mb-0 font-weight-bold">
-                      Devwares
-                    </span>
-                  </CDBLink>
-                  <span
-                    className="footer-rem"
-                    style={{
-                      fontSize: "3em",
-                      margin: "-2rem 0px -1.5rem 1rem",
-                      color: "#C4C4C4",
-                    }}
-                  >
-                    &#8226;
-                  </span>
-                  <small className="ml-2 mt-1">
-                    &copy; Devwares, 2020. All rights reserved.
-                  </small>
-                </div>
-                <div className="footer-rem">
-                  <CDBBtn
-                    flat
-                    color="dark"
-                    className="py-1 px-2 bg-dark border-0"
-                  >
-                    <i className="fab fa-facebook-f"></i>
-                  </CDBBtn>
-                  <CDBBtn
-                    flat
-                    color="dark"
-                    className="mx-3 py-1 px-2 bg-dark border-0"
-                  >
-                    <i className="fab fa-twitter"></i>
-                  </CDBBtn>
-                  <CDBBtn
-                    flat
-                    color="dark"
-                    className="py-1 px-2 bg-dark border-0"
-                  >
-                    <i className="fab fa-instagram"></i>
-                  </CDBBtn>
-                </div>
-              </footer>
+                </Col>
+                <Col sm={12} lg={3}>
+                  <Players/>  
+                </Col>
+              </Row>
             </div>
           </div>
+          <footer className="footer">
+            <div className="d-flex align-items-center">
+              <CDBLink to="/" className="text-dark footer-link">
+                <img alt="logo" src="/img/pages/logo.png" width="25px" />
+                <span className="ml-4 lead mb-0 font-weight-bold">
+                  Devwares
+                </span>
+              </CDBLink>
+              <span
+                className="footer-rem"
+                style={{
+                  fontSize: "3em",
+                  margin: "-2rem 0px -1.5rem 1rem",
+                  color: "#C4C4C4",
+                }}
+              >
+                &#8226;
+              </span>
+              <small className="ml-2 mt-1">
+                &copy; Devwares, 2020. All rights reserved.
+              </small>
+            </div>
+            <div className="footer-rem">
+              <CDBBtn
+                flat
+                color="dark"
+                className="py-1 px-2 bg-dark border-0"
+              >
+                <i className="fab fa-facebook-f"></i>
+              </CDBBtn>
+              <CDBBtn
+                flat
+                color="dark"
+                className="mx-3 py-1 px-2 bg-dark border-0"
+              >
+                <i className="fab fa-twitter"></i>
+              </CDBBtn>
+              <CDBBtn
+                flat
+                color="dark"
+                className="py-1 px-2 bg-dark border-0"
+              >
+                <i className="fab fa-instagram"></i>
+              </CDBBtn>
+            </div>
+          </footer>
         </div>
-     
+      </div>
+    </div>
   );
 };
