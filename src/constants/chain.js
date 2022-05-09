34578db53ff5734a47e5dc8cf8a2c782e5c1efad
webpro2021 <kirill.smal2020@gmail.com>
2022-05-10 00:39:18 +0300

    change test to mainnet

--- src/constants/chain.js
+++ src/constants/chain.js
@@ -1,2 +1,2 @@
-// export const chainId = "0x38"; //BSC Mainnet
-export const chainId = "0x61"; //BSC Testnet
+export const chainId = "0x38"; //BSC Mainnet
+// export const chainId = "0x61"; //BSC Testnet
