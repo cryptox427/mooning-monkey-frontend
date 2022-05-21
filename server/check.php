4aad2926fc733a80a3006d39e65f37012e654f79
webpro2021 <kirill.smal2020@gmail.com>
2022-05-21 15:07:57 +0300

    fix: cors error

--- server/check.php
+++ server/check.php
@@ -1,8 +1,7 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
     require('db.php');
 $data = json_decode(file_get_contents("php://input"),true);
