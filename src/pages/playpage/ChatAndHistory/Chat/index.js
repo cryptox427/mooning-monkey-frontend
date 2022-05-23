27787571ca31ee2a1b43ff0f5f4f55b6ebba22ef
webpro2021 <kirill.smal2020@gmail.com>
2022-05-23 20:38:22 +0300

    modify: chat

--- src/pages/playpage/ChatAndHistory/Chat/index.js
+++ src/pages/playpage/ChatAndHistory/Chat/index.js
@@ -1,7 +1,10 @@
 import { IoMdSend } from 'react-icons/io';
 import { FiPaperclip } from 'react-icons/fi';
 import { FaPlus } from 'react-icons/fa';
+import { useState } from 'react';
 
+import {serverUrl} from '../../../../utils/constant'
+import {request} from '../../../../utils/request';
 import emoji from '../../../../assets/images/playpage/emoji.png';
 import country1 from '../../../../assets/images/playpage/country1.png';
 import country2 from '../../../../assets/images/playpage/country2.png';
@@ -10,32 +13,48 @@ import country4 from '../../../../assets/images/playpage/country4.png';
 import './index.scss';
 import TalkCell from './TalkCell';
 
-
+let messageCounter = null;
 const Chat = (props) => {
+    const [messages, setMessages] = useState([]);
+    const [meMessage, setMessage] = useState("");
+    
+    const getMesssage = async () => {
+        const res = await request('get', `${serverUrl}chat/getMessagesOnetime.php`);
+        setMessages(res.result.split('<///***br***///>'));
+        console.log("~~~~~~~~~~chatMessage", res.result.split('<///***br***///>'))
+    }
+    const continueGetMessage = () => {
+        if(messageCounter) {
+            window.clearInterval(messageCounter);
+        }
+        
+        messageCounter = window.setInterval(getMesssage, 2000);
+    }
+    continueGetMessage();
+    const sendMessage = () => {
+        request('get', `${serverUrl}chat/sendMessage.php?message=${meMessage}`);
+        console.log("~~~~~~~~~~sendMessage")
+    }
     return (
         <div className="chat">
             
             <div className="chat-talks">
                 <div className="chat-talks-content">
                     <div className="talks">
-                        <TalkCell t_time="12:00" t_name="madam" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="junior" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="madam" t_content="how are you"/>
-                        <TalkCell t_time="12:00" t_name="ker" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="erstews_sdfe" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="evbdfgrw" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="madam" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="junior" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="madam" t_content="how are you"/>
-                        <TalkCell t_time="12:00" t_name="ker" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="erstews_sdfe" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="evbdfgrw" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="madam" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="junior" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="madam" t_content="how are you"/>
-                        <TalkCell t_time="12:00" t_name="ker" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="erstews_sdfe" t_content="hello"/>
-                        <TalkCell t_time="12:00" t_name="evbdfgrw" t_content="hello"/>
+                    {
+                        messages.length > 0 &&
+                        messages.map((message, index) => {
+                                let messageParts = message.split(' | ');
+
+                                let time = messageParts[0];
+                                let username = messageParts[1];
+                                let messageText = messageParts[2];
+                                return (
+                                    <TalkCell t_time={time} t_name={username} t_content={messageText}/>
+                                )
+                            }
+                        )
+                    }
                     </div>
                     <div className="mask">
 
@@ -66,8 +85,8 @@ const Chat = (props) => {
                 <div className="full-relative">
                     <img src={emoji} className="emoji"></img>
                     <FiPaperclip className="paper-clip"/>
-                    <input className="message-write-content" placeholder="Type something here..."></input>
-                    <div className="send-button">
+                    <input onChange={(e) => setMessage(e.target.value)} value={meMessage} className="message-write-content" placeholder="Type something here..."></input>
+                    <div className="send-button" onClick={()=>sendMessage()}>
                         <IoMdSend/>
                     </div>
                     <FaPlus className="plus-button"/>
